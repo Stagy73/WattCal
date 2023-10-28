6c2b4700a882a310bc40ff13147e9693e4b273c7
Nils <nils.mehlhorn.fr@gmail.com>
2023-10-28 14:29:21 +0200

    vite config edited

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
+  build: {
+    rollupOptions: {
+      input: "frontend/src/main.jsx",
+      external: ["react", "react-dom"], // Add "react" and "react-dom" as external dependencies
+    },
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
   },
 });
