18e3377db68abc64151438bf48ede7be7de07059
Nils <nils.mehlhorn.fr@gmail.com>
2023-10-28 10:36:20 +0200

    vite.config modified

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: "frontend/src/main.jsx", // Update to main.jsx
+      external: ["react"], // Add "react" as an external dependency
     },
   },
 });
