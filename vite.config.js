1d8de145031f3860ae4398fdee8aed519f928629
Nils <nils.mehlhorn.fr@gmail.com>
2023-10-28 10:44:49 +0200

    vite config again

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react", "react-dom"],
+      input: "frontend/src/main.jsx", // Update to the correct entry point
     },
   },
+  // Other configuration options...
 });
