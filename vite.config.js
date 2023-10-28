6c824114d3c08d5aff3c07dd8a939679758655dc
Nils <nils.mehlhorn.fr@gmail.com>
2023-10-28 10:30:51 +0200

    build successfull

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: "frontend/src/main.js", // Update the path to match the location of your entry JavaScript file
+      input: "frontend/src/main.jsx", // Update to main.jsx
     },
   },
 });
